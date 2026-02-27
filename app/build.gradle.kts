a52ed31f64e6593c671868f9dc785a4828fc9714
tejari49 <148905876+tejari49@users.noreply.github.com>
2026-02-27 08:42:58 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.kotlin.serialization)
